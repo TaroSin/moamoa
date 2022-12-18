0b76353fb8f9d7edea7e36d095ec4bb056fae53b
TaroSin <twokst@naver.com>
2022-12-18 22:06:22 +0900

    기본 구성 ui 완료

--- app/src/main/java/com/javaproject/employinfo/MainActivity.kt
+++ app/src/main/java/com/javaproject/employinfo/MainActivity.kt
@@ -9,57 +9,90 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.motion.widget.MotionLayout
 
 class MainActivity : AppCompatActivity() {
+    var employType = "고용형태"
+    var locaction = "지역별"
+    var back = "뒤로가기"
 
     var selectedIndex: Int = 0;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val view = intent.getStringExtra("view")
+
+        if(view == employType) setContentView(R.layout.activity_employ)
+        else if(view == locaction) setContentView(R.layout.activity_local)
+        else setContentView(R.layout.activity_main)
 
         val motionLayout = findViewById<MotionLayout>(R.id.motion_container)
 
-        val v1 = findViewById<View>(R.id.v1)
-        val v2 = findViewById<View>(R.id.v2)
-        val v3 = findViewById<View>(R.id.v3)
-        val v4 = findViewById<View>(R.id.v4)
-        val v1Text = findViewById<TextView>(R.id.v1_text);
-        val v2Text = findViewById<TextView>(R.id.v2_text);
-//        val v3Text = findViewById<TextView>(R.id.v3_text);
-//        val v4Text = findViewById<TextView>(R.id.v4_text);
-
         var enterCheck = 0
-        v1.setOnClickListener {
-            if (enterCheck==0){
-                if (selectedIndex == 0) {
-                    enterCheck=1
-                    return@setOnClickListener
+
+        val lastChildIdx = motionLayout.childCount - 2
+        val viewList = arrayListOf<View>()
+
+        for(i : Int in 0..lastChildIdx){
+            viewList.add(findViewById<View>(resources.getIdentifier("v"+(i+1), "id", this.packageName)))
+
+            if(i==0){
+                viewList[i].setOnClickListener{
+                    if (enterCheck==0){
+                        if (selectedIndex == 0) {
+                            enterCheck=1
+                            return@setOnClickListener
+                        }
+
+                        motionLayout.setTransition(R.id.s2, R.id.s1)
+                        motionLayout.transitionToEnd()
+                        selectedIndex = 0;
+                    }
+                    else{
+                        val textId = resources.getIdentifier("v"+(i+1)+"_text", "id", this.packageName)
+                        val text = viewList[i].findViewById<TextView>(textId).text
+
+                        if(text.equals(employType) or text.equals(locaction)){
+                            val intent = Intent(this, MainActivity::class.java)
+                            intent.putExtra("view", text)
+                            startActivity(intent)
+                        }else{
+                            val intent = Intent(this, NewActivity::class.java)
+                            intent.putExtra("view", text)
+                            startActivity(intent)
+                        }
+
+                        enterCheck=0;
+                    }
                 }
+            }else{
+                viewList[i].setOnClickListener {
+                    enterCheck=0
 
-                motionLayout.setTransition(R.id.s2, R.id.s1)
-                motionLayout.transitionToEnd()
-                selectedIndex = 0;
+                    if (selectedIndex == i){
+                        val textId = resources.getIdentifier("v"+(i+1)+"_text", "id", this.packageName)
+                        val text = viewList[i].findViewById<TextView>(textId).text
+
+                        if(text.equals(back)){
+                            finish()
+                        }else if(text.equals(employType) or text.equals(locaction)){
+                            val intent = Intent(this, MainActivity::class.java)
+                            intent.putExtra("view", text)
+                            startActivity(intent)
+                        }else{
+                            val intent = Intent(this, NewActivity::class.java)
+                            intent.putExtra("view", text)
+                            startActivity(intent)
+                        }
+
+                        return@setOnClickListener
+                    }
+
+                    val sFrom = resources.getIdentifier("s"+i, "id", this.packageName)
+                    val sTo = resources.getIdentifier("s"+(i+1), "id", this.packageName)
+                    motionLayout.setTransition(sFrom, sTo)
+                    motionLayout.transitionToEnd()
+
+                    selectedIndex = i
+                }
             }
-            else{
-                val intent = Intent(this, EmploActivity::class.java)
-                startActivity(intent)
-                enterCheck=0;
-            }
-
-        }
-
-        v2.setOnClickListener {
-            enterCheck=0
-            if (selectedIndex == 1){
-                val intent = Intent(this, LocalActivity::class.java)
-                startActivity(intent)
-                return@setOnClickListener
-            }
-
-
-                motionLayout.setTransition(R.id.s1, R.id.s2)
-            motionLayout.transitionToEnd()
-            selectedIndex = 1
-
-
         }
 
     }
